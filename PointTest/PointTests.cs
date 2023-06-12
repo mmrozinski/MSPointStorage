1605be051c90bd6c8d3982884f6a3ba4b3985834
mmrozinski <mmrozinski@student.agh.edu.pl>
2023-06-12 14:38:59 +0200

    Add cube volume calculation

--- PointTest/PointTests.cs
+++ PointTest/PointTests.cs
@@ -3,6 +3,7 @@ using MSPointStorage;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net.WebSockets;
 
 namespace PointTests
 {
@@ -55,6 +56,19 @@ namespace PointTests
             var expectedDistance = 5196.152422706632;
 
             var distance = Point.CalculateDistance(point1, point2);
+
+            Assert.AreEqual(expectedDistance, distance, 0.00000001);
+        }
+
+        [TestMethod]
+        public void TestCubeVolumeCalculation() 
+        {
+            var point1 = new Point(0, 0, 0);
+            var point2 = new Point(1, 1, 1);
+
+            var expectedDistance = 1.0;
+
+            var distance = Point.CalculateCubeVolume(point1, point2);
 
             Assert.AreEqual(expectedDistance, distance, 0.00000001);
         }
